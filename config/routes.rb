e5ce1b31a7a2fffb2f9d0921824523087474d967
Guest_ <Guest_@Hiroakis-MacBook-Pro.local>
2019-06-25 18:19:39 +0900

    finished chapter7

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get '/contact', to: "static_pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+
+  post '/signup',  to: 'users#create'
 end
